06e99e4bf14e4e9ba9861b1caa3e38a396a8cdb9
enggar appkey <enggar.appkey@gmail.com>
2021-12-22 10:46:50 +0800

    chore: add tailwindcss

--- app/root.jsx
+++ app/root.jsx
@@ -2,13 +2,18 @@ import {
   Links,
   LiveReload,
   Meta,
-  Outlet,
   Scripts,
-  ScrollRestoration
 } from "remix";
 
 export function meta() {
   return { title: "New Remix App" };
+}
+
+// setup style css
+import styles from "./tailwind.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
 }
 
 export default function App() {
@@ -21,8 +26,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
+        <div>
+          <p className="font-sans font-bold text-blue-600">Hello world</p>
+        </div>
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
